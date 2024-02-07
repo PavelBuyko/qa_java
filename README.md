2b2ac18aa6e1a9cab27da0d7ffa3917c8e835da1
PavelBuyko <145721578+PavelBuyko@users.noreply.github.com>
2024-02-07 18:27:03 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,9 +1,2 @@
 # qa_java
 QA Java Project
-
-Stack:
-junit  <version>4.13.2</version>
-org.mockito <version>5.10.0</version>
-jacoco-maven-plugin  <version>0.8.11</version>
-org.apache.maven.plugins version>3.8.1</version>
-
---
+++ src/test/java/LionHasManeTest.java
@@ -0,0 +1,33 @@
+import com.example.Feline;
+import com.example.Lion;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import static org.junit.Assert.*;
+
+@RunWith(Parameterized.class)
+public class LionHasManeTest {
+    private final String EXEPTIONMESSAGE = "Используйте допустимые значения пола животного - самец или самка";
+private final String sex;
+public LionHasManeTest(String sex)
+{
+    this.sex = sex;
+}
+    @Parameterized.Parameters
+    public static String[] getSex() {
+        return new String[]{"Самец", "Самка", "Вертолёт"};
+    }
+    @Test
+    public void constructLionTest()
+    {
+        try {
+            Feline feline = new Feline();
+            Lion lion = new Lion(sex,feline);
+            if (sex.equals("Самец")) {
+                assertTrue(lion.doesHaveMane());
+            } else                                   //Тут нет смысла делать ещё 1-у проверку , потому что если значение не будет является "Самец" или "Самка", то сразу же после конструктора мы провалимся в эксепшен.
+            { assertFalse(lion.doesHaveMane());}
+        }
+        catch (Exception e){assertEquals(e.getMessage(),EXEPTIONMESSAGE);}
+    }
+  }
